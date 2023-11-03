3cf16a887460fa448f5115e39fb8a53b4050ea25
freelancersazal <freelancersazal@gmail.com>
2023-11-04 01:44:20 +0600

    final update

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+
+    "use strict";
+
+    //======menu fix js======
+    if ($('.main_menu').offset() != undefined) {
+        var navoff = $('.main_menu').offset().top;
+        $(window).scroll(function () {
+            var scrolling = $(this).scrollTop();
+
+            if (scrolling > navoff) {
+                $('.main_menu').addClass('menu_fix');
+            } else {
+                $('.main_menu').removeClass('menu_fix');
+            }
+        });
+    }
+
+
+    //======NICE SELECT=======
+    $('.select_js').niceSelect();
+
+
+
+});
